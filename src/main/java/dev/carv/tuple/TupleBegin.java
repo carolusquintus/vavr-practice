428fbf816597a1a7ce6d4e9180068e8e9aa73de8
carolusquintus <carloschars@gmail.com>
2022-05-28 11:43:45 -0500

    Correction of naming variables on TupleBegin to keep consistency

--- src/main/java/dev/carv/tuple/TupleBegin.java
+++ src/main/java/dev/carv/tuple/TupleBegin.java
@@ -1,9 +1,7 @@
 package dev.carv.tuple;
 
 import io.vavr.Tuple;
-
 import java.util.AbstractMap.SimpleEntry;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,17 +11,17 @@ public class TupleBegin {
     }
 
     public static void main(String[] args) {
-        var t2 = Tuple.of("Alex", 36);
-        log.debug("{}", t2);
+        var t0 = Tuple.of("Alex", 36);
+        log.debug("{}", t0);
 
-        var t3 = Tuple.of(42, "Hello", new Customer("Alex", 36));
-        log.debug("{}", t3._1);
-        log.debug("{}", t3._2);
-        log.debug("{}", t3._3);
+        var t1 = Tuple.of(42, "Hello", new Customer("Alex", 36));
+        log.debug("{}", t1._1);
+        log.debug("{}", t1._2);
+        log.debug("{}", t1._3);
 
-        log.debug("{}", t3._1());
-        log.debug("{}", t3._2());
-        log.debug("{}", t3._3());
+        log.debug("{}", t1._1());
+        log.debug("{}", t1._2());
+        log.debug("{}", t1._3());
 
         var tEntry = Tuple.of(new SimpleEntry(42, "foo"));
         log.debug("{}", tEntry._1);
