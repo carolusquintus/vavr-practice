6256d6e1c1fabc757ff6d033a272a4440564589b
carolusquintus <carloschars@gmail.com>
2022-06-02 23:25:29 -0500

    Managing side effects with peek(), onEmpty() from Option & ifPresentOrElse() from Optional

---
+++ src/main/java/dev/carv/option/OptionSideEffects.java
@@ -0,0 +1,31 @@
+package dev.carv.option;
+
+import io.vavr.control.Option;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.function.Consumer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OptionSideEffects {
+
+    public static void main(String[] args) {
+
+        var r = new Random();
+
+        Consumer<Integer> lucky =  i -> log.debug("Felling lucky {}", i);
+        Runnable badLuck        =  () -> log.debug("{}", "Bad luck");
+
+        Option.of(r.nextBoolean() ? r.nextInt() : null)
+            .peek(lucky)
+            .onEmpty(badLuck);
+
+        Optional.ofNullable(r.nextBoolean() ? r.nextInt() : null)
+            .ifPresentOrElse(
+                lucky,
+                badLuck);
+
+    }
+}
